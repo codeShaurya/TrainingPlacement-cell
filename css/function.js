2a7aee6151e92034d11919c546f08e96a1e524df
Shubham Maurya <code.shaurya@gmail.com>
2017-09-05 01:06:41 +0530

    All the pages were styled in one direction

---
+++ css/function.js
@@ -0,0 +1,8 @@
+
+
+function newPopup(url) {
+    popupWindow=window.open(
+        url,'popUpWindow','height=350 ,width=810,left=10,top=10,resizable=yes,scrollbars=yes,toolbars=yes ,menubar=no ,location=no,directories=no,status=yes'
+    )
+
+}
